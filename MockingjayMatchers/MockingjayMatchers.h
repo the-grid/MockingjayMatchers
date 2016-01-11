9d28344e1ff86993f8272bfae718a802034114e8
Paul Young <paulyoungonline@gmail.com>
2016-01-11 14:39:19 -0800

    Add initial project with Mac target.

---
+++ MockingjayMatchers/MockingjayMatchers.h
@@ -0,0 +1,19 @@
+//
+//  MockingjayMatchers.h
+//  MockingjayMatchers
+//
+//  Created by Paul Young on 1/11/16.
+//  Copyright © 2016 The Grid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MockingjayMatchers.
+FOUNDATION_EXPORT double MockingjayMatchersVersionNumber;
+
+//! Project version string for MockingjayMatchers.
+FOUNDATION_EXPORT const unsigned char MockingjayMatchersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MockingjayMatchers/PublicHeader.h>
+
+
