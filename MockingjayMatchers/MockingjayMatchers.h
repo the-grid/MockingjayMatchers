0bb78e0c01ff95524837e1b503700ba01a4f27aa
Paul Young <paulyoungonline@gmail.com>
2016-01-11 15:35:38 -0800

    Add iOS target, and rename.

--- MockingjayMatchers/MockingjayMatchers.h
+++ MockingjayMatchers/MockingjayMatchers.h
@@ -1,12 +1,4 @@
-//
-//  MockingjayMatchers.h
-//  MockingjayMatchers
-//
-//  Created by Paul Young on 1/11/16.
-//  Copyright © 2016 The Grid. All rights reserved.
-//
-
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for MockingjayMatchers.
 FOUNDATION_EXPORT double MockingjayMatchersVersionNumber;
@@ -15,5 +7,3 @@ FOUNDATION_EXPORT double MockingjayMatchersVersionNumber;
 FOUNDATION_EXPORT const unsigned char MockingjayMatchersVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <MockingjayMatchers/PublicHeader.h>
-
-
