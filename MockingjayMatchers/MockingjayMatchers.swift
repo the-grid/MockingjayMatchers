313d1f61be3c6f7d6a92e23ddc0c4459d5c5834f
Paul Young <paulyoungonline@gmail.com>
2016-01-13 13:39:08 -0800

    Remove currying syntax.
    *
    https://github.com/apple/swift-evolution/blob/299eae16cff74b02f2ace08a77
    79cc72d6da2d24/proposals/0002-remove-currying.md

--- MockingjayMatchers/MockingjayMatchers.swift
+++ MockingjayMatchers/MockingjayMatchers.swift
@@ -1,57 +1,65 @@
 import Foundation
 import Mockingjay
 
-public func api(method: HTTPMethod, _ uri: String)(request: NSURLRequest) -> Bool {
-    guard let headers = request.allHTTPHeaderFields,
-        accept = headers["Accept"],
-        acceptEncoding = headers["Accept-Encoding"],
-        contentType = headers["Content-Type"] where
-        accept == "application/json" &&
-        contentType == "application/json" &&
-        acceptEncoding == "gzip;q=1.0,compress;q=0.5"
-    else {
-        return false
+public func api(method: HTTPMethod, _ uri: String) -> (request: NSURLRequest) -> Bool {
+    return { request in
+        guard let headers = request.allHTTPHeaderFields,
+            accept = headers["Accept"],
+            acceptEncoding = headers["Accept-Encoding"],
+            contentType = headers["Content-Type"] where
+            accept == "application/json" &&
+            contentType == "application/json" &&
+            acceptEncoding == "gzip;q=1.0,compress;q=0.5"
+        else {
+            return false
+        }
+        
+        return http(method, uri: uri)(request: request)
     }
-    
-    return http(method, uri: uri)(request: request)
 }
 
-public func api(method: HTTPMethod, _ uri: String, token: String)(request: NSURLRequest) -> Bool {
-    guard let headers = request.allHTTPHeaderFields,
-        authorization = headers["Authorization"] where
-        authorization == "Bearer \(token)"
-    else {
-        return false
+public func api(method: HTTPMethod, _ uri: String, token: String) -> (request: NSURLRequest) -> Bool {
+    return { request in
+        guard let headers = request.allHTTPHeaderFields,
+            authorization = headers["Authorization"] where
+            authorization == "Bearer \(token)"
+        else {
+            return false
+        }
+        
+        return api(method, uri)(request: request)
     }
-    
-    return api(method, uri)(request: request)
 }
 
-public func api(method: HTTPMethod, _ uri: String, body: [String: AnyObject])(request: NSURLRequest) -> Bool {
-    // https://github.com/kylef/Mockingjay/issues/32
-    guard let stream = request.HTTPBodyStream else { return false }
-    
-    stream.open()
-    
-    guard let streamJsonObject = try? NSJSONSerialization.JSONObjectWithStream(stream, options: NSJSONReadingOptions()) else {
-        return false
+public func api(method: HTTPMethod, _ uri: String, body: [String: AnyObject]) -> (request: NSURLRequest) -> Bool {
+    return { request in
+        // https://github.com/kylef/Mockingjay/issues/32
+        guard let stream = request.HTTPBodyStream else { return false }
+        
+        stream.open()
+        
+        guard let streamJsonObject = try? NSJSONSerialization.JSONObjectWithStream(stream, options: NSJSONReadingOptions()) else {
+            return false
+        }
+        
+        guard let streamDictionary = streamJsonObject as? [String: AnyObject] else { return false }
+        
+        let sortedStreamDictionary = sortDictionary(streamDictionary)
+        let sortedBody = sortDictionary(body)
+        
+        let bodyStreamJsonData = try? NSJSONSerialization.dataWithJSONObject(sortedStreamDictionary, options: NSJSONWritingOptions())
+        let bodyJsonData = try? NSJSONSerialization.dataWithJSONObject(sortedBody, options: NSJSONWritingOptions())
+        
+        guard bodyStreamJsonData == bodyJsonData else { return false }
+        
+        return api(method, uri)(request: request)
     }
-    
-    guard let streamDictionary = streamJsonObject as? [String: AnyObject] else { return false }
-    
-    let sortedStreamDictionary = sortDictionary(streamDictionary)
-    let sortedBody = sortDictionary(body)
-    
-    let bodyStreamJsonData = try? NSJSONSerialization.dataWithJSONObject(sortedStreamDictionary, options: NSJSONWritingOptions())
-    let bodyJsonData = try? NSJSONSerialization.dataWithJSONObject(sortedBody, options: NSJSONWritingOptions())
-    
-    guard bodyStreamJsonData == bodyJsonData else { return false }
-    
-    return api(method, uri)(request: request)
 }
 
-public func api(method: HTTPMethod, _ uri: String, token: String, body: [String: AnyObject])(request: NSURLRequest) -> Bool {
-    return api(method, uri, token: token)(request: request) && api(method, uri, body: body)(request: request)
+public func api(method: HTTPMethod, _ uri: String, token: String, body: [String: AnyObject]) -> (request: NSURLRequest) -> Bool {
+    return { request in
+        return api(method, uri, token: token)(request: request) && api(method, uri, body: body)(request: request)
+    }
 }
 
 private func sortDictionary(dictionary: [String: AnyObject]) -> [String: AnyObject] {
